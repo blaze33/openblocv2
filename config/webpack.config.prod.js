775fffae391fd78320242c74dffaaa6aaebd744e
Maxime Rouyrre <rouyrre+git@gmail.com>
2017-09-30 13:23:15 +0200

    Added gh-pages

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -153,6 +153,11 @@ module.exports = {
               compact: true,
             },
           },
+          {
+            test: /\.scss$/,
+            include: paths.appSrc,
+            loaders: ["style-loader", "css-loader", "sass-loader"]
+          },
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
