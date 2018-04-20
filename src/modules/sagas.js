93848855262af36f3401688f9aee1acdf54881b5
Maxime Rouyrre <rouyrre+git@gmail.com>
2018-04-20 02:21:50 +0200

    updated ghost client_secret, again

--- src/modules/sagas.js
+++ src/modules/sagas.js
@@ -1,10 +1,10 @@
 import { call, put, takeEvery } from 'redux-saga/effects'
 
 class GhostAPI {
-  constructor(props) {
+  constructor (props) {
     this.blogUrl = 'https://blog.openbloc.fr'
     this.blogApiUrl = `${this.blogUrl}/ghost/api/v0.1`
-    this.authData = 'client_id=ghost-frontend&client_secret=bb06581e7cc4'
+    this.authData = 'client_id=ghost-frontend&client_secret=18e5b4ad1180'
   }
 
   fetchPosts = () => {
@@ -17,27 +17,26 @@ class GhostAPI {
       json => json.posts
     )
   }
-  
 }
 
 const GhostAPIClient = new GhostAPI()
 
 // worker Saga: will be fired on USER_FETCH_REQUESTED actions
-function* fetchPosts(action) {
-   try {
-      const posts = yield call(GhostAPIClient.fetchPosts);
-      yield put({type: "POSTS_FETCH_SUCCEEDED", posts});
-   } catch (e) {
-      yield put({type: "POSTS_FETCH_FAILED", message: e.message});
-   }
+function * fetchPosts (action) {
+  try {
+    const posts = yield call(GhostAPIClient.fetchPosts)
+    yield put({type: 'POSTS_FETCH_SUCCEEDED', posts})
+  } catch (e) {
+    yield put({type: 'POSTS_FETCH_FAILED', message: e.message})
+  }
 }
 
 /*
   Starts fetchUser on each dispatched `USER_FETCH_REQUESTED` action.
   Allows concurrent fetches of user.
 */
-function* mySaga() {
-  yield takeEvery("POSTS_FETCH_REQUESTED", fetchPosts);
+function * mySaga () {
+  yield takeEvery('POSTS_FETCH_REQUESTED', fetchPosts)
 }
 
 export default mySaga
